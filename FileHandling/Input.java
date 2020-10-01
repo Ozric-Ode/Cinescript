13121cc6d1c5a117500c18bd8c25e6c3a9942795
Chirag-Mathur <chiragmathurindia@gmail.com>
2020-10-02 00:14:15 +0530

    Adding File Handling Features

--- FileHandling/Input.java
+++ FileHandling/Input.java
@@ -1,5 +1,28 @@
 package FileHandling;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 public class Input {
-    
+    public static void main(String[] args) {
+        try {
+            File myObj = new File("C:\\Users\\Chirag\\CPPFiles\\CodeBusta-Language\\Test\\TestCode.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                if (data.charAt(0) == 'p') {
+                    // Call print class from Features Package
+                }
+            }
+            myReader.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occured.");
+            e.printStackTrace();
+
+        }
+
+    }
+
 }
