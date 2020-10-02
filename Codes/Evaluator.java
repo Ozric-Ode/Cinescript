aec81368e6aab43fad87ffb3d1a49dc3b184d030
vidit2512 <201951167@iiitvadodara.ac.in>
2020-10-02 20:36:58 +0530

    update the evaluator and print

--- Codes/Evaluator.java
+++ Codes/Evaluator.java
@@ -6,37 +6,40 @@ import java.util.Stack;
 
 public class Evaluator 
 { 
-	public static int evaluate(String expression) 
+	public static int evaluate(String expression1) 
 	{ 
+		String expression="";
+		for(int i=0;i<expression1.length();i++)
+		expression=expression+" "+expression1.charAt(i);
+
 		char[] tokens = expression.toCharArray(); 
 
-		// Stack for numbers: 'values' 
+		 
 		Stack<Integer> values = new Stack<Integer>(); 
 
-		// Stack for Operators: 'ops' 
+	
 		Stack<Character> ops = new Stack<Character>(); 
 
 		for (int i = 0; i < tokens.length; i++) 
 		{ 
-			// Current token is a whitespace, skip it 
+			
 			if (tokens[i] == ' ') 
 				continue; 
 
-			// Current token is a number, push it to stack for numbers 
+			
 			if (tokens[i] >= '0' && tokens[i] <= '9') 
 			{ 
 				StringBuffer sbuf = new StringBuffer(); 
-				// There may be more than one digits in number 
+			
 				while (i < tokens.length && tokens[i] >= '0' && tokens[i] <= '9') 
 					sbuf.append(tokens[i++]); 
 				values.push(Integer.parseInt(sbuf.toString())); 
 			} 
-
-			// Current token is an opening brace, push it to 'ops' 
+ 
 			else if (tokens[i] == '(') 
 				ops.push(tokens[i]); 
 
-			// Closing brace encountered, solve entire brace 
+	
 			else if (tokens[i] == ')') 
 			{ 
 				while (ops.peek() != '(') 
@@ -44,32 +47,25 @@ public class Evaluator
 				ops.pop(); 
 			} 
 
-			// Current token is an operator. 
+		
 			else if (tokens[i] == '+' || tokens[i] == '-' || 
 					tokens[i] == '*' || tokens[i] == '/') 
 			{ 
-				// While top of 'ops' has same or greater precedence to current 
-				// token, which is an operator. Apply operator on top of 'ops' 
-				// to top two elements in values stack 
+				
 				while (!ops.empty() && hasPrecedence(tokens[i], ops.peek())) 
 				values.push(applyOp(ops.pop(), values.pop(), values.pop())); 
 
-				// Push current token to 'ops'. 
 				ops.push(tokens[i]); 
 			} 
 		} 
 
-		// Entire expression has been parsed at this point, apply remaining 
-		// ops to remaining values 
+	
 		while (!ops.empty()) 
 			values.push(applyOp(ops.pop(), values.pop(), values.pop())); 
 
-		// Top of 'values' contains result, return it 
 		return values.pop(); 
 	} 
 
-	// Returns true if 'op2' has higher or same precedence as 'op1', 
-	// otherwise returns false. 
 	public static boolean hasPrecedence(char op1, char op2) 
 	{ 
 		if (op2 == '(' || op2 == ')') 
@@ -80,8 +76,7 @@ public class Evaluator
 			return true; 
 	} 
 
-	// A utility method to apply an operator 'op' on operands 'a' 
-	// and 'b'. Return the result. 
+	
 	public static int applyOp(char op, int b, int a) 
 	{ 
 		switch (op) 
