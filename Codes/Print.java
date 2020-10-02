b1c7e37481247a6901c072e13162074ba0c61f90
vidit2512 <201951167@iiitvadodara.ac.in>
2020-10-02 18:19:15 +0530

    update print java file

--- Codes/Print.java
+++ Codes/Print.java
@@ -7,40 +7,54 @@ import java.io.IOException; // Import the IOException class to handle errors
 import features.*;
 
 class Print {
-    public void print(String s) {
+    public void print(String s1) {
         
         //+ ====> Concatenation
-        //'' ===> String in print function
-        Stack<Character> mainStack = new Stack<Character>();
-        String s1 = "";
-        String s2 = "";
-        int i=6;
+		//'' ===> String in print function
+		String s=s1.substring(6,s1.size()-3);
+		Stack<Character> mainStack = new Stack<Character>();
+		String s_cnc=" ";
+		String s_exp=" ";
+		String s_final=" ";
+
+
+       
+      
         boolean isStarted = false;
-            for (int j = i; j < s.length(); j++) {
-                if (s.charAt(j) == '(' && isStarted == false) {
-                    mainStack.push((Character) s.charAt(j));
-                    isStarted = true;
-                    j++;
-                    continue;
-                }
-                if (s.charAt(j) == (char) 34) {
-                    if (isStarted == false) {
-                        isStarted = true;
-                        continue;
-                    } else if (isStarted == true) {
-                        isStarted = false;
-                    }
-                }
-                if (s.charAt(j) == '+' && isStarted==false) {
-                    continue;
-                }
-                if (s.charAt(j) == ')' && isStarted == false) {
-                    mainStack.pop();
-                    break;
-                }
-                if (s.charAt(j) != (char) 34 && isStarted==true) {
-                    s2 = s2.concat(Character.toString(s.charAt(j)));
-                }
+            for (int j = 0; j < s.length(); j++) {
+
+				if(s.charAt(j)==(char)34)
+				{
+					if(mainStack.empty())
+					{mainStack.push((Character) s.charAt(j));}
+
+					else
+					{
+
+						s_final=s_final+s_cnc;
+						s_cnc="";
+						mainStack.pop();
+					}
+				continue;
+				}
+
+				if(!mainStack.empty())
+				{
+					s_cnc=s_cnc+s.charAt(j);
+				}
+				else
+				{
+					if(s.charAt(j)==',')
+					{
+						s_final=s_final+Evaluator.evaluate(s_exp);
+						s_exp="";
+					}
+					else
+					s_exp=s_exp+s.charAt(j);
+
+				}
+
+
             }
         
         try {
