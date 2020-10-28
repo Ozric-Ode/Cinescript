b31ff6184b20290ae63239950048b476ce033e7c
Monikinderjit Singh <52850691+Monik09@users.noreply.github.com>
2020-10-28 19:03:20 +0530

    If_else statements

--- Codes/Core.java
+++ Codes/Core.java
@@ -1,17 +1,26 @@
 package filehandling;
 
-//import filehandling.*;
+import features.Conditon;
 
 public class Core {
-    public void command(String data) {
+    public boolean command(String data) {
         if (data.charAt(0) == 'e') {
             //PrintInfo.print(data);
             int length = data.length();
             String data1 = data.substring(6,length-2);
             PrintInfo.print(data1);
             System.out.println(data1);
-
         }
+		if(data.charAt(0)=='i'){
+			int length = data.length();
+			if(data.charAt(length-1)=='{'){
+				length--;
+			}
+			String data1=data.substring(3, length);
+			Conditon con=new Conditon();
+			return con.checkCondition(data1.trim()); 
+		}
+		return false;
     }
 
 }
