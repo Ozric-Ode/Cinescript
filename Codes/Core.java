3fd8e618c210348c4fd172da30f4948349b017d6
Chirag-Mathur <chiragmathurindia@gmail.com>
2020-10-29 00:18:47 +0530

    see only

--- Codes/Core.java
+++ Codes/Core.java
@@ -1,26 +1,106 @@
 package filehandling;
 
+import java.util.*;
 import features.Conditon;
+import filehandling.Precore;
 
 public class Core {
-    public boolean command(String data) {
-        if (data.charAt(0) == 'e') {
-            //PrintInfo.print(data);
-            int length = data.length();
-            String data1 = data.substring(6,length-2);
-            PrintInfo.print(data1);
-            System.out.println(data1);
+    Set<String> set = new HashSet<String>();
+
+    public void command(String data) {
+        set.add("elaan");
+        set.add("agartummiljao");
+        set.add("warna");
+
+        System.out.println(data);
+        String keyword = "";
+        String data2;
+        int indd;
+
+        for (int i = 0; i < data.length();) {
+
+            keyword = "";
+            while (data.charAt(i) != '(') {
+                keyword += data.charAt(i);
+                i++;
+
+            }
+            data2 = "";
+            if (set.contains(keyword)) {
+                if (keyword.charAt(0) == 'e') {
+                    indd = i;
+                    while (data.charAt(i) != ';') {
+                        data2 += data.charAt(i);
+                        i++;
+                    }
+                    int length = data2.length();
+                    String data1 = data2.substring(1, length - 1);
+                    // System.out.println(data1+"%%%");
+                    // PrintInfo.print(data1);
+                    System.out.println(data1);
+
+                }
+                if (keyword.charAt(0) == 'a') {
+                    data2 += keyword + '(';
+                    while (data.charAt(i) != ')') {
+                        data2 += data.charAt(i);
+                        i++;
+                    }
+                    data2 += ')';
+                    i++;
+                    data2+='{';
+                    String str2 = data.substring(i);
+                    int len2 = Precore.extract(str2).length();
+                    data2 += Precore.extract(str2);
+                    i += len2;
+                    data2+='}';
+
+                    keyword = "";
+                    while (data.charAt(i) != '(') {
+                        keyword += data.charAt(i);
+                        i++;
+
+                    }
+                    if (set.contains(keyword)) {
+                        data2 += keyword + '(';
+                        while (data.charAt(i) != ')') {
+                            data2 += data.charAt(i);
+                            i++;
+                        }
+                        data2 += ')';
+                        i++;
+                        data2+='{';
+                        str2 = data.substring(i);
+                        len2 = Precore.extract(str2).length();
+                        data2 += Precore.extract(str2);
+                        i += len2;
+                        data2+='}';
+                    }
+
+                }
+                
+            }
+            i++;
+
         }
-		if(data.charAt(0)=='i'){
-			int length = data.length();
-			if(data.charAt(length-1)=='{'){
-				length--;
-			}
-			String data1=data.substring(3, length);
-			Conditon con=new Conditon();
-			return con.checkCondition(data1.trim()); 
-		}
-		return false;
+
+        // if (data.charAt(0) == 'e') {
+        // //PrintInfo.print(data);
+        // int length = data.length();
+        // String data1 = data.substring(6,length-2);
+        // PrintInfo.print(data1);
+        // System.out.println(data1);
+        // }
+        // if(data.charAt(0)=='i'){
+        // int length = data.length();
+        // if(data.charAt(length-1)=='{'){
+        // length--;
+        // }
+        // String data1=data.substring(3, length);
+        // Conditon con=new Conditon();
+        // return con.checkCondition(data1.trim());
+        // }
+        // return false;
     }
 
 }
--- Codes/Input.java
+++ Codes/Input.java
@@ -7,56 +7,21 @@ import java.util.*;
 public class Input {
     public static void main(String[] args) {
         Core core = new Core();
-		try {
-			File myObj = new File("..\\Test\\TestCode4.txt");
+        try {
+            File myObj = new File("..\\Test\\TestCode.txt");
             Scanner myReader = new Scanner(myObj);
-			boolean isBracketed=false,isIf=false;
-			boolean result=false;
-			String data;
+            boolean isBracketed = false, isIf = false;
+            boolean result = false;
+            String data;
+            String code = "";
             while (myReader.hasNextLine()) {
 
-	//---MONIKINDERJITSINGH-SAURAVGUPTA----- IF-ELSE PART-----STARTING-
-				data = myReader.nextLine().trim();
-			forIf: {
-						if(data.charAt(0)=='i' ){
-							isIf=true;
-						if(data.charAt(data.length()-1)=='{'){
-							result=core.command(data);
-							System.out.println(result);
-							while(data.charAt(data.length()-1)!='}'){
-								data=myReader.nextLine().trim();
-								if(result)
-									core.command(data);
-							}
-							// System.out.println("ko");
-							try{
-								data=myReader.nextLine().trim();
-							}
-							catch(Exception e){}
-							// isBracketed=true;
-						}
-						if(isIf==true && result==false && data.charAt(0)=='w'){
-							data=data.trim();
-							
-							System.out.println(data);
-						if(result==false && data.charAt(data.length()-1)=='{'){
-							while( data.charAt(data.length()-1)!='}'){
-								data=myReader.nextLine().trim();
-								if(result==false)
-									core.command(data);
-							}
-						}
-					}
-				}
-			}
-	//---MONIKINDERJITSINGH-SAURAVGUPTA----- IF-ELSE PART----------------END
-				
-				if(isIf==false && data.charAt(0)=='e' ){
-					core.command(data);
-				}
+                data = myReader.nextLine();
+                code = code + data;
 
             }
             myReader.close();
+            core.command(code);
 
         } catch (FileNotFoundException e) {
             System.out.println("An error occured.");
