121be928558c529e6d05d38452f05e733e041f10
vidit2512 <201951167@iiitvadodara.ac.in>
2020-10-28 19:25:25 +0530

    add precore.java

---
+++ Codes/Precore.java
@@ -0,0 +1,21 @@
+public class Precore
+{
+    public static String extract ( String s)
+    {
+        int k=1;String sfinal="";
+        for(int i=1;i<s.length();i++)
+        {
+            if(s.charAt(i)=='{')k++;
+            else if(s.charAt(i)=='}')k--;
+            
+            if(k==0)
+            return sfinal;
+            else sfinal=sfinal+s.charAt(i);
+
+        }
+       
+        return sfinal;
+
+    }
+ 
+}
