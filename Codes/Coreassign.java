934275062aa97496dbc9185509375955b4f9f494
Chirag-Mathur <chiragmathurindia@gmail.com>
2020-11-01 00:44:00 +0530

    Add coreassign

---
+++ Codes/Coreassign.java
@@ -0,0 +1,46 @@
+package datatypes;
+
+import java.util.*;
+
+import features.Evaluator;
+
+public class Coreassign {
+	public static void coreassign(String str){
+		String var="";
+		String expression="";
+		boolean isEquality=false;
+		int j=0;
+		str=str.trim();
+		while(j!=str.length()-1 && str.charAt(j)!=';'){
+			if(str.charAt(j)=='='){
+				isEquality=true;
+				j++;
+				continue;
+			}
+			if(isEquality==false){
+				var+=str.charAt(j++);
+			}
+			else{
+				expression+=str.charAt(j++);
+			}
+		}
+		//assuming class name to be Coredeclare
+		//assuming map of ints name to be intmap
+		var=var.trim();
+		expression=expression.trim();
+		
+		if(Coredeclare.intmap.containsKey(var)==true){
+			if(Corescope.scope_variables.contains(var)==true){
+				int result=Evaluator.evaluate(expression);
+				Coredeclare.intmap.replace(var, result); 
+			}
+			else{
+				//throw error of scope
+			}
+		}
+		else{
+			//throw error of non-availability of variable
+		}
+
+	}
+}
