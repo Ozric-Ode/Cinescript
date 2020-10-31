c0c6080c53ee37b2183d50f4c7acd8a41f99480a
Monikinderjit Singh <52850691+Monik09@users.noreply.github.com>
2020-10-31 21:31:10 +0530

    Update onetime.bat

--- Codes/onetime.bat
+++ Codes/onetime.bat
@@ -4,7 +4,7 @@ ECHO Enter c to compile and run all files or r to run the Input [c/r]!
 set /p choice=""
 if %choice%==c (
 ECHO Program is being compiled.
-javac -d . .\*.java
+javac -d . *.java
 java filehandling.Input)
 if %choice%==r (
 java filehandling.Input)
